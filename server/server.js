7a84c2195aa80b99cc2ebe4be24e65b3e71b520f
Alexander Apostolov <alexanderapostolov@protonmail.com>
2017-10-02 18:00:49 -0400

    Add GET /todos:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -34,6 +35,26 @@ app.get('/todos', (req, res) => {
       res.status(400).send(e);
     }
   );
+});
+
+app.get('/todos/:id', (req, res) => {
+  // Pull id dynamically off of url
+  const id = req.params.id;
+  // Validate id
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo }); // By sending it back in an object, you're future proofing for additional properties
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
 });
 
 app.listen(3000, () => {
