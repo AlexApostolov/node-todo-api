3650b65f7f236624cf9e0a0c8225a4ec1452f60b
Alexander Apostolov <alexanderapostolov@protonmail.com>
2017-10-02 15:29:23 -0400

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,24 +24,20 @@ app.post('/todos', (req, res) => {
   );
 });
 
+app.get('/todos', (req, res) => {
+  // Get all todos
+  Todo.find().then(
+    todos => {
+      res.send({ todos });
+    },
+    e => {
+      res.status(400).send(e);
+    }
+  );
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
 
 module.exports = { app };
-// Run Todo model as a constructor function to create a todo instance.
-// Mongoose will automatically lowercase & pluralize Todo => todos as a collection name
-// var newTodo = new Todo({
-//   text: 'Cook dinner'
-// });
-
-// The new todo isn't added until it's saved. Saving returns a promise.
-// newTodo.save().then(
-//   doc => {
-//     // Get todo
-//     console.log('Saved todo', doc);
-//   },
-//   e => {
-//     console.log('Unable to save todo');
-//   }
-// );
