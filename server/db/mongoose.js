292f962bfadbbb9e8efcf6a7f7995613fe6c4104
Alexander Apostolov <alexanderapostolov@protonmail.com>
2017-10-03 10:04:55 -0400

    Set up app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 // Use built in JS Promises
 mongoose.Promise = global.Promise;
 // Mongoose won't continue on behind the scenes until after it has finished connecting to the DB
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'
+);
 
 module.exports = { mongoose };
