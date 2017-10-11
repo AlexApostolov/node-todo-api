c73a83ca5f8a0affa17f0f34687a1483fd137d81
Alexander Apostolov <alexanderapostolov@protonmail.com>
2017-10-11 16:06:23 -0400

    Add tests for POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -222,12 +222,14 @@ describe('POST /users', () => {
           return done(err);
         }
         // Find a user where the email equals the email above
-        User.findOne({ email }).then(user => {
-          expect(user).toExist();
-          // Expect that the password does NOT match since it has NOT been hashed
-          expect(user.password).toNotBe(password);
-          done();
-        });
+        User.findOne({ email })
+          .then(user => {
+            expect(user).toExist();
+            // Expect that the password does NOT match since it has NOT been hashed
+            expect(user.password).toNotBe(password);
+            done();
+          }) // Get a useful error message for which assertion failed & why, instead of just timing out
+          .catch(e => done(e));
       });
   });
   it('should return validation errors if request invalid', done => {
@@ -245,5 +247,55 @@ describe('POST /users', () => {
       .send({ email: users[0].email, password: 'Password123!' })
       .expect(400)
       .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', done => {
+    request(app)
+      .post('/users/login')
+      .send({ email: users[1].email, password: users[1].password })
+      .expect(200)
+      .expect(res => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id)
+          .then(user => {
+            expect(user.tokens[0]).toInclude({
+              access: 'auth',
+              token: res.headers['x-auth']
+            });
+            done();
+            // Like other async tests, we want to catch the error if the tokens are not equal
+          })
+          .catch(e => done(e));
+      });
+  });
+  it('should reject invalid login', done => {
+    request(app)
+      .post('/users/login')
+      .send({ email: users[1].email, password: 'smellOfFailure' })
+      .expect(400)
+      .expect(res => {
+        expect(res.headers['x-auth']).toNotExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id)
+          .then(user => {
+            expect(user.tokens.length).toBe(0);
+            done();
+            // Like other async tests, we want to catch the error if the tokens are not equal
+          })
+          .catch(e => done(e));
+      });
   });
 });
